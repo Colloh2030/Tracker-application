7c43277de59040ecfcc9f4c076945e8ad5657dca
Stephen.Wambua <stephen.wambua@ncbagroup.com>
2024-12-16 02:33:41 +0300

    modified the the files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h>My Daily Activity Tracking Application</h>
+          
         </p>
         
       </header>
