1c6e8a47ca0ca0bf82bd1d1a69325b04fe547f0e
Stephen.Wambua <stephen.wambua@ncbagroup.com>
2024-12-16 00:11:39 +0300

    added mananja yetu

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          tracker application
+          Mananja Yetu
         </a>
       </header>
     </div>
