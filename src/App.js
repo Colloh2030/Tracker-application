bc777481ea834f0c3a8696da17dcaaa4b2f9ce48
Stephen.Wambua <stephen.wambua@ncbagroup.com>
2024-12-16 02:58:08 +0300

    organizing the Website

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import './components/Navbar';
 import './components/Footer'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <>Navbar</>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h>My Daily Activity Tracking Application</h>
-          
-        </p>
-        <>Footer</>
-        
-      </header>
+    <div>
+      <Navbar/>
+    </div>,
+    <div>
+      <Footer/>
+     
     </div>
+    
   );
 }
 
